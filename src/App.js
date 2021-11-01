7f8ae1834778f6fa58579100cf1fbbd085abdd35
kevinszuchet <kevinszuchet@gmail.com>
2021-11-01 10:03:49 +0200

    visual form to calculate compound interest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Formik, Form } from 'formik';
+import Container from './components/Container';
+import Section from './components/Section';
+import Input from './components/Input';
+import Button from './components/Button';
 
 function App() {
+  const handleSubmit = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: '',
+          }}
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form>
+            <Input name="deposit" label="Initial deposit" />
+            <Input name="contribution" label="Anual contribution" />
+            <Input name="years" label="Years" />
+            <Input name="rate" label="Interest rate" />
+            <Button>Calculate</Button>
+          </Form>
+        </Formik>
+      </Section>
+    </Container>
   );
 }
 
